c61479ea7fab842641e3ecd41292ce943c81af4d
ienming <hsieh.alan.0726@gmail.com>
2021-03-06 22:51:17 +0800

    change api to https for testing

--- src/components/Page1.vue
+++ src/components/Page1.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <p v-for="datum of data" :key="datum.SiteName">{{ datum.SiteName }}</p>
+    <span>1111</span>
+    <!-- <p v-for="datum of data" :key="datum.SiteName">{{ datum.SiteName }}</p> -->
   </div>
 </template>
 
@@ -22,11 +23,12 @@ export default {
     document.title = "Page1"
     
     // Test for API
-    let api = "http://opendata2.epa.gov.tw/AQI.json";
+    let api = "https://data.epa.gov.tw/api/v1/aqx_p_432?format=xml&limit=50&api_key=f8146cb1-193c-4a9a-812a-9c84c9ef87b9&filters=County,EQ,%E8%87%BA%E5%8D%97%E5%B8%82";
     Vue.axios.get(api).then((response) => {
       //response is an JSON object
-      console.log(Array.isArray(response.data));
-      this.data = response.data;
+      // console.log(Array.isArray(response.data));
+      // this.data = response.data;
+      console.log(response);
     });
   },
 };
